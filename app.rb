c4e2d94b01d40bfa3540fa8314b9ac16de037a2c
Farya <farya.hsn@gmail.com>
2021-10-30 20:36:05 +0500

    added player choice to game class - connected to controller

--- app.rb
+++ app.rb
@@ -8,38 +8,28 @@ class RPS < Sinatra::Base
     register Sinatra::Reloader
     enable :sessions
 
-  # before do
-  #   @player = Player.instance
-  #   @game = Game.instance
-  # end
-
   get '/' do
     erb :index
   end
 
   post '/names' do
     $player = Player.new(params[:player])
-    # computer = 'Computer'
-    # $game = Game.new(player, computer)
     redirect '/play'
   end
 
   get '/play' do
     @player = $player.name
-    # @game = $game
     erb :play
   end
 
   post '/play' do
-    session[:choose] = params[:choose]
-    @game = Game.create(@player, @choice)
+    $choice = Game.new(params[:choose])
     redirect '/chosen'
   end
 
-  get '/chosen' do
-    # @game = $game
-    @choice = session[:choose]
-    # @game.winner(@choice)
+ get '/chosen' do
+    @choice = $choice.choice
+    @player = $player.name
     erb :chosen
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,27 +1,25 @@
 class Game
 
-  attr_accessor :c_choice, :p_choice, :players
+  attr_accessor :choice
 
-  def initialize(player, p_choice, computer = 'computer', c_choice = ["Rock", "Paper", "Scissors"])
-    @players = [player, computer]
-    @p_choice = p_choice
-    @c_choice = c_choice.sample
+  def initialize(choice)
+    @choice = choice
   end
 
-  def self.create()
-    @game = Game.new(@players[0], @p_choice, @players[1], @c_choice)
-  end
+  # def self.create()
+  #   @game = Game.new(@players[0], @p_choice, @players[1], @c_choice)
+  # end
 
-  def self.instance
-    @game
-  end
+  # def self.instance
+  #   @game
+  # end
 
-  def winner
-    wins = { "Rock" => "Scissors", "Scissors" => "Paper", "Paper" => "Rock" }
-    case
-      when wins[@p_choice] == @c_choice then "#{@players[0]} wins!"
-      when wins[@c_choice] == @p_choice then "#{@players[1]} wins!"
-    end
-  end
+  # def winner
+  #   wins = { "Rock" => "Scissors", "Scissors" => "Paper", "Paper" => "Rock" }
+  #   case
+  #     when wins[@p_choice] == @c_choice then "#{@players[0]} wins!"
+  #     when wins[@c_choice] == @p_choice then "#{@players[1]} wins!"
+  #   end
+  # end
 
 end
